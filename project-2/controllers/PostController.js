5208d92c3a98d437d8313b317eaee5ae872b4b2d
Steven Fernández <vsteven25@gmail.com>
2021-10-25 22:41:40 -0600

    Add posts creation page

--- project-2/controllers/PostController.js
+++ project-2/controllers/PostController.js
@@ -1,9 +1,35 @@
 "use strict";
 
+const Post = require('../models/index.js').Post;
+const Sequelize = require('sequelize');
+
 exports.form = (req, res) => {
-    res.render("new_post", { title: "New post" });
+    res.render('post_form', {title: 'Create post'});
 }
 
-exports.create = (req, res) => {
-    console.log(req.body);
+exports.create = async (req, res) => {
+    await Post.create({
+        title: req.body.title,
+        abstract: req.body.abstract,
+        text: req.body.text,
+        image: req.body.image,
+        UserId: req.user.id,
+        CategoryName: 'PHP' // TODO: Un-hardcode
+    });
+    res.redirect('/posts');
+}
+
+exports.list = async (req, res) => {
+    let page = req.query.page;
+    let offset = typeof page == 'undefined' ? 0 : (page - 1) * 5;
+
+    const posts = await Post.findAndCountAll({
+        limit: 5,
+        offset: offset
+    });
+
+    res.render('posts', {
+        title: 'Posts',
+        posts: posts.rows
+    });
 }
--- project-2/routes/Post.js
+++ project-2/routes/Post.js
@@ -7,5 +7,6 @@ const secured = require('./Secured');
 
 router.get("/new", secured, controller.form);
 router.post("/new", secured, controller.create);
+router.get("/", secured, controller.list);
 
 module.exports = router;
