929e996d3ab9741a37921067fbff7862ca0fade6
Steven Fernández <vsteven25@gmail.com>
2021-10-26 22:23:01 -0600

    Add comments, categories and authors links

--- project-2/controllers/PostController.js
+++ project-2/controllers/PostController.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const Post = require('../models/index.js').Post;
-const Sequelize = require('sequelize');
+const User = require('../models/index.js').User;
+const Category = require('../models/index.js').Category;
 
 exports.form = (req, res) => {
     res.render('post_form', {title: 'Create post'});
@@ -20,23 +21,43 @@ exports.create = async (req, res) => {
 }
 
 exports.list = async (req, res) => {
-    let page = typeof req.query.page == 'undefined' ? 1 : req.query.page;
+    let page = 0;
+
+    if(typeof req.query.page === 'undefined') {
+        page = 1;
+    }
+    else {
+        page = parseInt(req.query.page);
+        page = isNaN(page) ? 1 : page;
+    }
+
     let offset = (page - 1) * 5;
 
     const posts = await Post.findAndCountAll({
         limit: 5,
-        offset: offset
+        offset: offset,
+        include: User
     });
 
     const count = Math.ceil(posts.count / 5);
     const startPage = page - 5 < 1 ? 1 : page - 5;
     const endPage = page + 5 > count ? count : page + 5;
-    const pages = Array(endPage - startPage + 1).fill().map((_, idx) => startPage + idx)
+    const pages = Array(endPage - startPage + 1).fill().map((_, idx) => startPage + idx);
+
+    const categories = await Category.findAll();
+    const users = await User.findAll();
+
+    for(const post of posts.rows) {
+        post.commentsCount = await post.countComments();
+    }
     
     res.render('posts', {
         title: 'Posts',
         posts: posts.rows,
-        pages: pages
+        pages: pages,
+        active: page,
+        categories: categories,
+        users: users
     });
 }
 
