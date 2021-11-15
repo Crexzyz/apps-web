3eee728f730aefcb471ca04e2354f88be953ad6e
Steven Fernández <vsteven25@gmail.com>
2021-11-15 15:28:56 -0600

    Remove login wall on comments

--- project-2/routes/Comment.js
+++ project-2/routes/Comment.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const controller = require('../controllers/CommentController');
 const Customs = require('./Customs');
 
-router.post('/:id', Customs.loggedInOnly, controller.create);
+router.post('/:id', controller.create);
 router.post('/delete/:id', Customs.loggedInOnly, Customs.authorizePostAction, controller.delete);
 
 module.exports = router;
